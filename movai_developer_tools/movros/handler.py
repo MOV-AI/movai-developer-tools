bb28d9cfba7e51b574aacee194d1890dc0533b74
mithun <51120171+MithunKinarullathil@users.noreply.github.com>
2022-05-18 18:45:03 +0100

    Comment out extra arg test as there are no extra args Fix imports in test handler

--- movai_developer_tools/movros/handler.py
+++ movai_developer_tools/movros/handler.py
@@ -1,6 +1,5 @@
 """Main package module. Contains the handler, executors and other modules inside.# noqa: E501"""
 import argparse
-import imp
 import sys
 
 import movai_developer_tools.utils.logger as logging
