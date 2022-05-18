1935f28421f85fb8d555b828e2c4a11562663c6b
mithun <51120171+MithunKinarullathil@users.noreply.github.com>
2022-05-18 17:55:37 +0100

    Update name, author, description and package url in setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r") as fh:
 # The name of the package is the one to be used in runtime.
 # The 'install_requires' is where you specify the package dependencies of your package. They will be automaticly installed, before your package.  # noqa: E501
 setuptools.setup(
-    name="dummy-component",
+    name="movai-developer-tools",
     version="1.0.0-2",
-    author="DevOps team",
-    author_email="devops@mov.ai",
-    description="Dummy description",
+    author="Mithun Kinarullathil",
+    author_email="mithun@mov.ai",
+    description="Scripts to improve development process when using MOV.AI",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/MOV-AI/repository-template-python-component",
+    url="https://github.com/MOV-AI/movai-developer-tools",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=["Programming Language :: Python :: 3"],
