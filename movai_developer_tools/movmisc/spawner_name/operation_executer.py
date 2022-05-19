5202925c724568a352cc3de9a810b4f4bb93e811
mithun <51120171+MithunKinarullathil@users.noreply.github.com>
2022-05-19 09:58:37 +0100

    Add spawner_name command to movmisc

---
+++ movai_developer_tools/movmisc/spawner_name/operation_executer.py
@@ -0,0 +1,24 @@
+"""Module where all the behaviour of a command should be destributed."""
+import movai_developer_tools.utils.logger as logging
+
+
+class OperationExecuter:
+    """Main class to expose ros topics, services and parameters from docker to the host"""
+
+    def __init__(self):
+        """If your executor requires some initialization, use the class constructor for it"""
+        logging.info("Init")
+
+    def execute(self, args):
+        """Method where the main behaviour of the executer should be"""
+        logging.info("execute behaviour")
+        logging.info(args)
+        logging.info("TODO: Please complete me")
+
+    @staticmethod
+    def add_expected_arguments(parser):
+        """Method exposed for the handle to append our executer arguments."""
+        # parser.add_argument(
+        #     "--executor_specific_arg",
+        #     help="specific argument needed for this executor",
+        # )
