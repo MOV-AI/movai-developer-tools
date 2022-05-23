e3cc96fdca948fd885f892cf34d1d1e2fc941d00
mithun <51120171+MithunKinarullathil@users.noreply.github.com>
2022-05-23 20:09:03 +0100

    Get spawner name to execute import commands in

--- movai_developer_tools/movbkp/import_metadata/operation_executer.py
+++ movai_developer_tools/movbkp/import_metadata/operation_executer.py
@@ -1,5 +1,6 @@
 """Module where all the behaviour of a command should be destributed."""
 from movai_developer_tools.utils import logger as logging
+from movai_developer_tools.movmisc.spawner.operation_executer import Spawner
 
 
 class Importer:
@@ -8,10 +9,15 @@ class Importer:
     def __init__(self):
         """If your executor requires some initialization, use the class constructor for it"""
         logging.debug("Importer Init")
+        self.spawner = Spawner()
 
     def execute(self, args):
         """Method where the main behaviour of the executer should be"""
         logging.debug(f"Execute importer behaviour with args: {args}")
+        # Get container to make the operation in silently
+        args.silent = True
+        spawner_container_name = self.spawner.get_spawner_name(args)
+        logging.error(spawner_container_name)
 
     @staticmethod
     def add_expected_arguments(parser):
