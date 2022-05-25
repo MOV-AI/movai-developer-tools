c69b32b00ec95ca7fb14ccf7ced2e9d9275524ee
mithun <51120171+MithunKinarullathil@users.noreply.github.com>
2022-05-25 13:23:08 +0100

    Temporary save

--- movai_developer_tools/movros/expose_network/operation_executer.py
+++ movai_developer_tools/movros/expose_network/operation_executer.py
@@ -14,6 +14,8 @@ class ExposeNetwork:
         # ROS instllation dir
         self.ros_install_dir = "/opt/ros"
 
+    # def get_file_docker(self, path: str) -> :
+
     def execute(self, args):
         """Method where the main behaviour of the executer should be"""
         logging.debug(f"Execute ExposeNetwork behaviour with args: {args}")
@@ -36,9 +38,6 @@ class ExposeNetwork:
         # Silence the log output from other services because the function is being used internally
         # Only the return value is used and not the printed one
         args.silent = True
-        # Get name of the spawner container
-        args.sub_command = "name"
-        spawner_name = Spawner().execute(args)
         # Get ip of the spawner and ros-master container networks
         args.sub_command = "ip"
         spawner_ip = Spawner().execute(args)
@@ -47,14 +46,42 @@ class ExposeNetwork:
         args.sub_command = "gateway"
         spawner_gateway = Spawner().execute(args)
 
-        # TODO: Update docker entry point script: Add ROS_IP
+        # TODO: Update docker entry point script: Check and add export ROS_IP={spawner_ip}
+        # Get docker entry point file
+
+        # Check if the export ROS_IP is already in docker entrypoint, and export it if not
+        # if [[ $(docker exec -it "${container_name}" sh -c "grep -q 'export ROS_IP=${spawner_ip}' /usr/local/bin/docker-entrypoint.sh && echo 1 || echo 0" | grep -o -E '[0-1]+') == 0 ]]
+        # then
+        #     echo "ROS_IP Not exported in $(mo-spawner-name), exporting: ${spawner_ip}."
+        #     docker exec -ituroot "${container_name}" sh -c "sed -i -e '/set -e/a export ROS_IP=${spawner_ip}' /usr/local/bin/docker-entrypoint.sh"
+        #     # For the export to take effect, container needs restart
+        #     read -p "Container ${container_name} needs to be restarted for changes to take effect. Do you want to restart ${container_name} now? (Y/n)" -n 1 -r
+        #     echo
+        #     if [[ $REPLY =~ ^[Yy]$ ]]
+        #     then
+        #         docker restart "${container_name}" > /dev/null
+        #     else
+        #         echo "Skipping restart!"
+        #     fi
+        # fi
+
+        # # Check if in spawner's bashrc
+        # if [[ $(docker exec -it "${container_name}" sh -c "grep -q 'export ROS_IP=${spawner_ip}'  /opt/mov.ai/.bashrc && echo 1 || echo 0" | grep -o -E '[0-1]+') == 0 ]]
+        # then
+        #     docker exec -it "${container_name}" sh -c "echo '\nexport' ROS_IP=${spawner_ip} >> /opt/mov.ai/.bashrc"
+        # fi
+
+        # export ROS_IP="${host_ip}"
+        # export ROS_MASTER_URI="http://${master_ip}:11311/"
+        # echo "ROS environment variables ROS_IP and ROS_MASTER_URI exported. You can now use your ROS tools :)"
+
         # TODO: Update docker bashrc: Add ROS_IP
         # TODO: Export ROS_IP and ROS_MASTER_URI to the host
         # export ROS_IP="${gateway}"
         # export ROS_MASTER_URI="http://${ros_master_ip}:11311/"
         # echo "ROS environment variables ROS_IP and ROS_MASTER_URI exported. You can now use your ROS tools :)"
         logging.info(
-            f"spawner_name: {spawner_name}, spawner_ip: {spawner_ip}, ros_master_ip: {ros_master_ip}, spawner_gateway: {spawner_gateway}"
+            f"spawner_ip: {spawner_ip}, ros_master_ip: {ros_master_ip}, spawner_gateway: {spawner_gateway}"
         )
 
     @staticmethod
