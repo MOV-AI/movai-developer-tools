8f5e1376506ba9c1ab7d4d97cc0de1956801e1c9
mithun <51120171+MithunKinarullathil@users.noreply.github.com>
2022-06-02 15:11:15 +0100

    catkin_build Remove commented code, Use ContainerTools instead of Spawner class

--- movai_developer_tools/movros/catkin_build/operation_executer.py
+++ movai_developer_tools/movros/catkin_build/operation_executer.py
@@ -1,33 +1,35 @@
 """Module where all the behaviour of a command should be destributed."""
 from movai_developer_tools.utils import logger
-from movai_developer_tools.movmisc.spawner.operation_executer import Spawner
+from movai_developer_tools.utils.container_tools import ContainerTools
 
 
 class CatkinBuild:
-    """Main class to expose ros topics, services and parameters from docker to the host"""
+    """Main class to build ros workspace."""
 
-    def __init__(self):
-        """If your executor requires some initialization, use the class constructor for it"""
+    def __init__(self) -> None:
         logger.debug("CatkinBuild Init")
-        # ROS instllation dir
-        self.ros_ws_dir = "/opt/ros"
 
-    def execute(self, args):
+    def execute(self):
         """Method where the main behaviour of the executer should be"""
-        logger.info("Running catkin build in spawner")
+        # ROS instllation dir
+        # ros_install_dir = "/opt/ros"
 
-        # Use container exec to do catkin build in spawner
-        args.cmd = "cd /opt/mov.ai/workspaces/USER_ROS1 && catkin build -j2 -l2 --mem-limit 50%"
-        args.user = "movai"
-        args.env = []
-        args.silent = False
-        spawner = Spawner(args)
-        spawner.exec()
+        # Instanciate spawner container class
+        # Reg expressions for finding the spawner container
+        regex_spawner_name = "^spawner-.*"
+        # Instanciate for silent operation if silent arg is True, else take default
+        spawner = ContainerTools(regex_spawner_name, silent=True)
+        # Info to the user
+        logger.info(f"Running catkin build in spawner {spawner.get_name()}")
+
+        # # Use container exec to do catkin build in spawner
+        # args.cmd = "cd /opt/mov.ai/workspaces/USER_ROS1 && catkin build -j2 -l2 --mem-limit 50%"
+        # args.user = "movai"
+        # args.env = []
+        # args.silent = False
+        # spawner = Spawner(args)
+        # spawner.exec()
 
     @staticmethod
     def add_expected_arguments(parser):
         """Method exposed for the handle to append our executer arguments."""
-        # parser.add_argument(
-        #     "--executor_specific_arg",
-        #     help="specific argument needed for this executor",
-        # )
