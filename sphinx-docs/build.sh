f8e5b635a2afa02dbc6150fe4ad553784fd53bb9
mithun <51120171+MithunKinarullathil@users.noreply.github.com>
2022-06-03 14:06:28 +0100

    Add githubpages extension for sphinx. Modify cp command to add hidden files as well on build.sh

--- sphinx-docs/build.sh
+++ sphinx-docs/build.sh
@@ -7,5 +7,5 @@ sphinx-apidoc -f -o ./source ../movai_developer_tools
 # Build the HTML
 make -C . html
 # Copy html files to docs/
-echo "Copying html files from build/html/ to docs/"
-cp -R ./build/html/* ../docs/
+echo "Copying all files from build/html/ to docs/"
+cp -R ./build/html/. ../docs/
